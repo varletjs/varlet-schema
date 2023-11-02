0a5018be900a4b347244fae901a73e9c5407c04f
haoziqaq <357229046@qq.com>
2023-11-02 18:26:19 +0800

    style: format code

--- packages/varlet-schema-playground/src/useVarlet.ts
+++ packages/varlet-schema-playground/src/useVarlet.ts
@@ -2,37 +2,50 @@ import { SchemaRendererInjects, type SchemaRendererComponents } from '@varlet/sc
 import { shallowRef } from 'vue'
 import Varlet from '@varlet/ui'
 
-const FUNCTIONS = ['ImagePreview', 'Snackbar', 'Picker', 'ActionSheet', 'Dialog', 'Locale', 'StyleProvider', 'LoadingBar']
+const FUNCTIONS = [
+  'ImagePreview',
+  'Snackbar',
+  'Picker',
+  'ActionSheet',
+  'Dialog',
+  'Locale',
+  'StyleProvider',
+  'LoadingBar'
+]
 const EXCLUDES = ['Ripple', 'Lazy', 'Hover', 'version', 'install']
 
 export function useVarlet() {
-	const exports = Object.entries(Varlet as Record<string, any>)
+  const exports = Object.entries(Varlet as Record<string, any>)
 
-  const components = shallowRef(exports.reduce((components, [key, value]) => {
-		if (EXCLUDES.includes(key)) {
-			return components
-		}
+  const components = shallowRef(
+    exports.reduce((components, [key, value]) => {
+      if (EXCLUDES.includes(key)) {
+        return components
+      }
 
-		if (value.hasOwnProperty('Component')) {
-			components[key] = value.Component
-			return components
-		}
+      if (value.hasOwnProperty('Component')) {
+        components[key] = value.Component
+        return components
+      }
 
-		components[key] = value
+      components[key] = value
 
-		return components
-	}, {} as SchemaRendererComponents))
+      return components
+    }, {} as SchemaRendererComponents)
+  )
 
-  const injects = shallowRef(exports.reduce((injects, [key, value]) => {
-		if (FUNCTIONS.includes(key)) {
-			injects[key] = value
-		}
+  const injects = shallowRef(
+    exports.reduce((injects, [key, value]) => {
+      if (FUNCTIONS.includes(key)) {
+        injects[key] = value
+      }
 
-		return injects
-	}, {} as SchemaRendererInjects))
+      return injects
+    }, {} as SchemaRendererInjects)
+  )
 
-	return {
-		components,
-		injects
-	}
+  return {
+    components,
+    injects
+  }
 }
